985616d8b38d1ab4b9c1624e179d238a01498214
Your Name <you@example.com>
2025-10-28 01:33:20 +0300

    add files when create question and answer

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -4,33 +4,67 @@ feature 'Пользователь отвечает на вопрос' do
   given!(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
 
-  scenario 'Аутентифицированный пользователь отвечает на вопрос', js: true do
-    sign_in(user)
-    visit question_path(question)
-
-    fill_in 'answer_body', with: 'Тестовый текст ответа'
-    click_on 'Post Answer'
-
-    sleep 2
-
-    expect(page).to have_css('.answers')
-    within '.answers' do
-      expect(page).to have_content 'Тестовый текст ответа'
+  context 'Аутентифицированный пользователь' do
+    before do
+      sign_in(user)
+      visit question_path(question)
     end
-    expect(page).to have_field('answer_body', with: '')
+
+    scenario 'Пользователь отвечает на вопрос', js: true do
+      sign_in(user)
+      visit question_path(question)
+
+      fill_in 'answer_body', with: 'Тестовый текст ответа'
+      click_on 'Post Answer'
+
+      expect(page).to have_css('.answers')
+      within '.answers' do
+        expect(page).to have_content 'Тестовый текст ответа'
+      end
+      expect(page).to have_field('answer_body', with: '')
+    end
+
+    scenario 'Пользователь пытается создать ответ с пустым телом', js: true do
+      sign_in(user)
+      visit question_path(question)
+
+      click_on 'Post Answer'
+
+      expect(page).to have_content "Body can't be blank"
+    end
+
+    scenario 'Пользователь отвечает на вопрос с одним прикрепленным файлом', js: true do
+      sign_in(user)
+      visit question_path(question)
+
+      fill_in 'answer_body', with: 'Ответ с файлом'
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      click_on 'Post Answer'
+
+      within '.answers' do
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
+
+    scenario 'Пльзователь отвечает на вопрос с несколькими прикрепленными файлами', js: true do
+      sign_in(user)
+      visit question_path(question)
+
+      fill_in 'answer_body', with: 'Ответ с файлами'
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Post Answer'
+
+      within '.answers' do
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
-  scenario 'Неаутентифицированный пользователь не может ответить на вопрос' do
-    visit question_path(question)
-    expect(page).to_not have_button 'Post Answer'
-  end
-
-  scenario 'Пользователь пытается создать ответ с пустым телом', js: true do
-    sign_in(user)
-    visit question_path(question)
-
-    click_on 'Post Answer'
-
-    expect(page).to have_content "Body can't be blank"
+  context 'Неаутентифицированный пользователь' do
+    scenario 'Пользователь не может ответить на вопрос' do
+      visit question_path(question)
+      expect(page).to_not have_button 'Post Answer'
+    end
   end
 end
--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -3,28 +3,51 @@ require 'rails_helper'
 feature 'Пользователь создает вопрос' do
   given(:user) { create(:user) }
 
-  scenario 'Аутентифицированный пользователь создает вопрос' do
-    sign_in(user)
-    visit questions_path
-    click_on 'New Question'
-    fill_in 'question_title', with: 'Тестовый заголовок вопроса'
-    fill_in 'question_body', with: 'Тестовый текст вопроса'
-    click_on 'Create Question'
-    expect(page).to have_content 'Тестовый заголовок вопроса'
-    expect(page).to have_content 'Тестовый текст вопроса'
+  context 'Аутентифицированный пользователь' do
+    before do
+      sign_in(user)
+      visit questions_path
+      click_on 'New Question'
+    end
+
+    scenario 'Пользователь создает вопрос' do
+      fill_in 'question_title', with: 'Тестовый заголовок вопроса'
+      fill_in 'question_body', with: 'Тестовый текст вопроса'
+      click_on 'Create Question'
+      expect(page).to have_content 'Тестовый заголовок вопроса'
+      expect(page).to have_content 'Тестовый текст вопроса'
+    end
+
+    scenario 'Пользователь создает вопрос с одним прикрепленным файлом' do
+      fill_in 'question_title', with: 'Вопрос с файлом'
+      fill_in 'question_body', with: 'Текст вопроса с файлом'
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      click_on 'Create Question'
+
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
+    scenario 'Пользователь создает вопрос с несколькими прикрепленными файлами' do
+      fill_in 'question_title', with: 'Вопрос с файлами'
+      fill_in 'question_body', with: 'Текст вопроса с файлами'
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Create Question'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
+    scenario 'Пользователь пытается создать вопрос с пустыми полями' do
+      click_on 'Create Question'
+      expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
-  scenario 'Неаутентифицированный пользователь пытается создать вопрос' do
-    visit questions_path
-    expect(page).to_not have_link 'Ask question'
-  end
-
-  scenario 'Пользователь пытается создать вопрос с пустыми полями' do
-    sign_in(user)
-    visit questions_path
-    click_on 'New Question'
-    click_on 'Create Question'
-    expect(page).to have_content "Title can't be blank"
-    expect(page).to have_content "Body can't be blank"
+  context 'Неаутентифицированный пользователь' do
+    scenario 'Неаутентифицированный пользователь пытается создать вопрос' do
+      visit questions_path
+      expect(page).to_not have_link 'New Question'
+    end
   end
 end
